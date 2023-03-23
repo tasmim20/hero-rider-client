48c23d858119ccaccf745218b00e19f47a69a2ed
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-03-23 16:24:27 +0600

    basic routes set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h2>hero alom</h2>
+       <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
