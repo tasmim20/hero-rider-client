a8a7d9693f89f93661e6f3dcf35783a10fcc9b78
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-03-23 16:15:14 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBneELbbfcY0aiOkS5VUCLACW69OHeSe0g",
+  authDomain: "hero-rider-4448d.firebaseapp.com",
+  projectId: "hero-rider-4448d",
+  storageBucket: "hero-rider-4448d.appspot.com",
+  messagingSenderId: "746319730938",
+  appId: "1:746319730938:web:1936a3f6074f004dd4d143"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
